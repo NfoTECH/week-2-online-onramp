8b107ed4d3c7dd79d04fafb630b2dee62aef4036
NfoTECH <fortunenwachukwu@gmail.com>
2022-04-06 17:41:54 +0100

    Algorithm answers modified

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,6 +1,18 @@
 function missingNumber(numArr) {
-
+    
+  let test = [1,2,3,4,5,6,7,8,9,10];
+  let output =[]
+  for (let i = 0; i < test.length; i++) {
+    let complete = test[i];
+    if(!numArr.includes(complete)) {
+      output.push(complete)
+    }
+  }
+  return output
 }
+
+
+
 console.log(missingNumber([1,2,3,4,6,7,8,9]))
 
 module.exports = missingNumber
--- algorithms/challenge-2.js
+++ algorithms/challenge-2.js
@@ -1,10 +1,13 @@
 function numSwap(number) {
     let numArr = number.toString().split('');
+                              console.log(numArr)
 
     for (let i = 0; i < numArr.length; i +=2) {
     let result = numArr [i];
+                              console.log(result)
     numArr[i] = numArr[i + 1];
     numArr[i + 1] = result;
+   
   }
   return Number(numArr.join(''))
 }
